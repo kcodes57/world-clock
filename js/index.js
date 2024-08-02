65a386b5158304fa5dcb79228d38bfdbe44d2e28
kcodes57 <kir652986@gmail.com>
2024-08-02 13:22:27 -0500

    updated code so that everything was in one function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function getLaInfo() {
+function updateTime() {
   let losAngelesDateElement = document.querySelector("#la-date");
   let losAngelesDate = moment()
     .tz("America/Los_Angeles")
@@ -6,17 +6,19 @@ function getLaInfo() {
   losAngelesDateElement.innerHTML = losAngelesDate;
 
   let losAngelesTimeElement = document.querySelector("#la-time");
-  let losAngelesTime = moment().tz("America/Los_Angeles").format("h:mm:ss A");
+  let losAngelesTime = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>] ");
   losAngelesTimeElement.innerHTML = losAngelesTime;
+
+  let sydneyDateElement = document.querySelector("#s-date");
+  let sydneyDate = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
+  sydneyDateElement.innerHTML = sydneyDate;
+
+  let sydneyTimeElement = document.querySelector("#s-time");
+  let sydneyTime = moment().tz("Australia/Sydney").format("h:mm:ss A");
+  sydneyTimeElement.innerHTML = sydneyTime;
 }
 
-getLaInfo();
-setInterval(getLaInfo, 1000);
-
-let sydneyDateElement = document.querySelector("#s-date");
-let sydneyDate = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
-sydneyDateElement.innerHTML = sydneyDate;
-
-let sydneyTimeElement = document.querySelector("#s-time");
-let sydneyTime = moment().tz("Australia/Sydney").format("h:mm:ss A");
-sydneyTimeElement.innerHTML = sydneyTime;
+updateTime();
+setInterval(updateTime, 1000);

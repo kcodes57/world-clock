ae8a89e08c5be49ad3e3816299fe2d7aeedae321
kcodes57 <kir652986@gmail.com>
2024-08-02 12:54:49 -0500

    added timezone data

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let losAngelesDateElement = document.querySelector("#la-date");
-let losAngelesDate = moment().format("MMMM Do YYYY");
+let losAngelesDate = moment()
+  .tz("America / Los_Angeles")
+  .format("MMMM Do YYYY");
 losAngelesDateElement.innerHTML = losAngelesDate;
 
 let sydneyDateElement = document.querySelector("#s-date");
-let sydneyDate = moment().format("MMMM Do YYYY");
+let sydneyDate = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
 sydneyDateElement.innerHTML = sydneyDate;
